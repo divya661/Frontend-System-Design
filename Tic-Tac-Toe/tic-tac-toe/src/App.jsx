4a0cc255977fa33dcd3f5cc5bf26afd48533f3b5
DIVYA CHAUHAN <divyac091@gmail.com>
2024-02-02 14:01:55 +0530

    fix: check for win logic

--- Tic-Tac-Toe/tic-tac-toe/src/App.jsx
+++ Tic-Tac-Toe/tic-tac-toe/src/App.jsx
@@ -1,125 +1,151 @@
-import {useState} from "react";
+import "./App.css";
+import { useState } from "react";
 
-const generateBoard = (size=3) => {
-  const newBoard = [];
-
-  for(let i=0;i<size;i++){
-    newBoard.push([...Array(size)]);
+const generateBoard = (size = 3) => {
+  const board = [];
+  for (let i = 0; i < size; i++) {
+    board.push([...Array(size)]);
   }
+  return board;
+};
 
-  return newBoard;
-}
+const maxNoOfMoves = 9;
 
-const checkHorizontal = (board) => {
-  for(let row of board){
+const checkHorizontalWin = (board) => {
+  for (let row of board) {
     const rowSet = new Set(row);
-    if(rowSet.size === 1 && !rowSet.has(undefined)){
+    if (rowSet.size === 1 && !rowSet.has(undefined)) {
       return true;
     }
   }
-}
+};
 
-const rowsToColumns = (board) => {
-  const newBoard = [];
-  let column = 0;
-  while(column < board.length){
-    const newRow = [];
-    for(let row=0;row<board.length;row++){
-      newRow.push(board[row][column]);
+const checkVerticalWin = (board, currentPlayer) => {
+  for (let cell = 0; cell < board.length; cell++) {
+    let totalCurrentPlayerMoves = 0;
+    for (let row = 0; row < board.length; row++) {
+      if (board[row][cell] === currentPlayer) {
+        totalCurrentPlayerMoves++;
+      }
     }
-    newBoard.push(newRow);
-    column++;
+    if (totalCurrentPlayerMoves === 3) {
+      return true;
+    }
   }
 
-  return newBoard;
-}
+  return false;
+};
 
-const diagonalToRow = (board) =>{ 
-  const newBoard = [[],[]];
-  let increment = 0;
-  let decrement = board.length-1;
-  while(increment < board.length){
-    newBoard[0].push(board[increment][increment]);
-    newBoard[1].push(board[increment][decrement]);
-    increment++;
-    decrement--;
+const checkDiagonalWin = (board, currentPlayer) => {
+  let moves = 0;
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][i] === currentPlayer) {
+      moves++;
+    }
   }
-  return newBoard;
-}
 
-const checkForWin = (board) => {
-  // horizontal
-  if(checkHorizontal(board)){
+  if (moves === 3) {
     return true;
   }
 
-   // vertical
-   if(checkHorizontal(rowsToColumns(board))){
-    return true;
-   }
-
-  // diagonal
-  if(checkHorizontal(diagonalToRow(board))){
+  moves = 0;
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][board.length - i - 1] === currentPlayer) {
+      moves++;
+    }
+  }
+  if (moves === 3) {
     return true;
   }
- 
-}
+
+  return false;
+};
+
+const checkForWins = (board, currentPlayer) => {
+  if (
+    checkHorizontalWin(board) ||
+    checkVerticalWin(board, currentPlayer) ||
+    checkDiagonalWin(board, currentPlayer)
+  ) {
+    return true;
+  }
+
+  return false;
+};
 
 function App() {
   const [board, setBoard] = useState(generateBoard(3));
-  const [currPlayer, setCurrPlayer] = useState('x');
-  const [currentNumberOfMoves,setCurrentNumberOfMoves] = useState(1);
-  const [maxNumberOfMoves,setMaxNumberOfMoves] = useState(9);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [currentNoOfMoves, setCurrentNoOfMoves] = useState(0);
+  const [winner, setWinner] = useState("");
 
-  // console.log(board);
- 
-  const handleClick = (row, col) => {
-    board[row][col] = currPlayer;
-    setBoard([...board]);
-    setCurrentNumberOfMoves(currentNumberOfMoves+1);
-    if(checkForWin(board)){
-      console.log(currPlayer + 'wins');
-      setBoard(generateBoard(3));
-      setCurrPlayer('x');
-    } else if(currentNumberOfMoves===maxNumberOfMoves){
-      console.log('Match draws');
-    } else {
-      setCurrPlayer(currPlayer === 'x' ? 'y' : 'x');
+  const handleCellClick = (rowIdx, cellIdx) => {
+    if (winner) {
+      return;
     }
-  }
+
+    if (currentNoOfMoves === maxNoOfMoves) {
+      setCurrentNoOfMoves(currentNoOfMoves + 1);
+      return;
+    }
+
+    if (currentNoOfMoves > maxNoOfMoves) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[rowIdx][cellIdx] = currentPlayer;
+    setBoard(newBoard);
+    setCurrentNoOfMoves((moves) => moves + 1);
+    if (checkForWins(newBoard, currentPlayer)) {
+      setWinner(currentPlayer);
+      console.log(currentPlayer + " wins match");
+    } else {
+      setCurrentPlayer((prev) => (prev === "X" ? "O" : "X"));
+    }
+  };
 
   return (
-    <div>
-      {
-        board.map((row,r) => {
+    <>
+      <div>
+        {board.map((row, rowIdx) => {
           return (
-            <div key={'rowCell_'+r}
-            style={{
-              display:'flex',
-            }}>{
-              row.map((cell,c) => {
+            <div key={"row_" + rowIdx} style={{ display: "flex" }}>
+              {row.map((cell, cellIdx) => {
                 return (
-                  <div 
-                  key={'cell_'+c} 
-                  onClick={()=>handleClick(r,c)}
-                  style={{
-                    border: 'solid white 1px',
-                    height: '50px',
-                    width: '50px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    color: 'white'
-                    }}>
-                     {cell} 
+                  <div
+                    key={"cell_" + cellIdx}
+                    onClick={() => handleCellClick(rowIdx, cellIdx)}
+                    style={{
+                      height: "50px",
+                      width: "50px",
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      color: "black",
+                      border: "1px solid black",
+                    }}
+                  >
+                    {cell}
                   </div>
-                )
-              })
-            }</div>
+                );
+              })}
+            </div>
           );
-        })
-}
-     </div>
+        })}
+      </div>
+      {winner && (
+        <div style={{ fontSize: "24px", padding: "12px", margin: "auto",color:'black' }}>
+          Winner is {winner}
+        </div>
+      )}
+      {currentNoOfMoves >= maxNoOfMoves && (
+        <div style={{ fontSize: "24px", padding: "12px", margin: "auto",color:'black' }}>
+          Match Draws
+        </div>
+      )}
+    </>
   );
 }
 
